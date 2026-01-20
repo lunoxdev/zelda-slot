6852ff8fd2aaadfa3b6c43b5ad8555a72a28ec1f
lunoxdev <lunox.code@gmail.com>
2026-01-20 09:30:32 -0600

    fix logo error

--- src/game/textures.ts
+++ src/game/textures.ts
@@ -2,7 +2,6 @@ import { Assets } from "pixi.js";
 
 export const loadTextures = async () => {
   const zeldaBg = await Assets.load("/assets/zelda-bg.webp");
-  const zeldaLogo = await Assets.load("/assets/zelda-logo.png");
   const shield = await Assets.load("/assets/zelda-shield.png");
   const link = await Assets.load("/assets/link.webp");
   const zelda = await Assets.load("/assets/zelda.webp");
@@ -11,7 +10,6 @@ export const loadTextures = async () => {
   return {
     zeldaBg,
     shield,
-    zeldaLogo,
     slotTextures: [link, zelda, ganon],
   };
 };
